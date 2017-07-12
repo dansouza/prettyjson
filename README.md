f154e5756b5ec78c6d4272d265242c13cd46f79b
Daniel Souza <thehazard@gmail.com>
2017-07-12 15:18:20 -0400

    updated README, added first version

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # prettyjson
 command-line utility to pretty-print JSON content from files/stdin
+
+# install
+```
+$ go install github.com/dansouza/prettyjson
+```
+
+# usage
+[![asciicast](https://asciinema.org/a/8o0kNockI6UJx2oJQhIroSdkR.png)](https://asciinema.org/a/8o0kNockI6UJx2oJQhIroSdkR)
---
+++ prettyjson.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+
+	"io"
+	"os"
+
+	"encoding/json"
+
+	"github.com/hokaccha/go-prettyjson"
+	"github.com/mattn/go-colorable"
+)
+
+func main() {
+	var content map[string]interface{}
+	var file io.Reader
+	var err error
+
+	if len(os.Args) == 2 {
+		if os.Args[1] != "-" {
+			file, err = os.Open(os.Args[1])
+			if err != nil {
+				fmt.Printf("error: %s: %s", os.Args[1], err)
+				os.Exit(-1)
+			}
+		}
+	} else {
+		file = os.Stdin
+	}
+
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&content)
+	if err != nil {
+		fmt.Printf("error decoding json: %s", err)
+		os.Exit(-1)
+	}
+
+	pretty, err := prettyjson.Marshal(content)
+	if err != nil {
+		fmt.Printf("error pretty-printing json: %s", err)
+		os.Exit(-1)
+	}
+
+	colorable.NewColorableStdout().Write(pretty)
+	fmt.Print("\n")
+}
